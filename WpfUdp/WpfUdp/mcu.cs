733a1d5141049c79f84d3cb94aa55dcb5c9865da
york.yu <404164127@qq.com>
2017-12-21 01:48:58 +0800

    增加mcu类，以及对应的单元测试。

---
+++ WpfUdp/WpfUdp/mcu.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Threading;
+using System.Diagnostics;
+
+namespace WpfUdp
+{
+    public sealed class Mcu
+    {
+        private static readonly Mcu _Instance = new Mcu();
+
+        static Mcu()
+        {
+            //构造函数
+        }
+
+        private Mcu()
+        {
+            Thread ThreadDebugInfo = new Thread(new ThreadStart(DebugInfoProc));
+            ThreadDebugInfo.IsBackground = true; //设置为后台线程
+            ThreadDebugInfo.Start();
+            //构造函数
+        }
+
+        public static Mcu Instance
+        {
+            get
+            {
+                return _Instance;
+            }
+        }
+
+        private void DebugInfoProc()
+        {
+            while (true)
+            {
+                Debug.WriteLine("worker thread: working...");
+                Thread.Sleep(1000);
+            }
+        }
+
+        public string UnitTest()
+        {
+            return "UnitTest";
+        }
+    }
+}
---
+++ WpfUdp/WpfUdpTests/McuTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WpfUdp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfUdp.Tests
+{
+    [TestClass()]
+    public class McuTests
+    {
+        [TestMethod()]
+        public void UnitTestTest()
+        {
+            Mcu myMcu = Mcu.Instance;
+            Assert.AreEqual(myMcu.UnitTest(), "UnitTest");
+        }
+    }
+}
